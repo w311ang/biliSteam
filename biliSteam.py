9334100b1cc7831eedd90806c4c0e05d4dc77a80
RZ <30381083+w311ang@users.noreply.github.com>
2021-03-30 21:00:31 +0800

    Update biliSteam.py

--- biliSteam.py
+++ biliSteam.py
@@ -3,6 +3,7 @@ import re
 import pytools
 import os
 import pickle
+import markdown
 
 vlist=requests.get('http://api.bilibili.com/x/space/arc/search?mid=518876755&pn=1&ps=10').json()['data']['list']['vlist']
 checked=[]
@@ -20,6 +21,9 @@ for one in vlist:
   if not bvid in checked:
     des=requests.get('http://api.bilibili.com/x/web-interface/archive/desc?bvid=%s'%bvid).json()['data']
     link=re.findall('(?:https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]',des)
+    re='https://weidian.com/?userid=1025773260&wfr=mkt-item-detail&spider_token=7038'
+    if re in link:
+      link.remove(re)
     bv=one['bvid']
     umake.append({'title':one['title'],'des':des,'link':link,'bv':bv})
     checked.append(bv)
@@ -29,12 +33,12 @@ for one in umake:
   title=one['title']
   link=one['link']
   bvlink='https://bilibili.com/video/'+one['bv']
-  content+='=====%s=====\n'%title
-  content+=bvlink+'\n'
-  content+='----------\n'
+  md+='## %s\n'%title
+  md+=bvlink+'\n'
+  md+='---\n'
   for alink in link:
-    content+=alink+'\n'
-  content+='\n'
+    md+=alink+'\n'
+  content=markdown.markdown(md)
 
 with open('checked.txt','wb') as file:
   pickle.dump(checked,file)
