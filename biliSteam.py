928e3a7ac15ee5f0b42c8dc9918a5f706155f667
RZ <30381083+w311ang@users.noreply.github.com>
2021-06-02 22:38:31 +0800

    Update biliSteam.py

--- biliSteam.py
+++ biliSteam.py
@@ -25,7 +25,7 @@ for one in vlist:
     des=requests.get('http://api.bilibili.com/x/web-interface/archive/desc?bvid=%s'%bvid).json()['data']
     try:
       link=re.search('(?<==\n)([\s\S]+)',des).group()
-    except AttributeError,Argument:
+    except AttributeError, Argument:
       link=Argument
     link=link.replace('请相信我们的视频质量，值得你的关注！','')
     link=link.replace('\n','\n\n')
